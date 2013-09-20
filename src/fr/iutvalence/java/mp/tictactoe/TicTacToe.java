95b3f35612785548f066a8a5dd766c37f8fb7d94
SeneclauzePierre <pierre.seneclauze@iut-valence.fr>
2013-09-20 10:00:22 +0200

    Class "TicTacToe" created
    Creation of a new class "TicTacToe", used to start and handle a new
    game.

---
+++ src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
@@ -0,0 +1,18 @@
+package fr.iutvalence.java.mp.tictactoe;
+
+/**
+ * TicTacToe
+ * 
+ * v0.0
+ * 
+ * 2013-09-20
+ * 
+ * @author seneclap
+ *
+ * This class is used to start and handle a new game.
+ * 
+ */
+public class TicTacToe
+{
+    
+}
