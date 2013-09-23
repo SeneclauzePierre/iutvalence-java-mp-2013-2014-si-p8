b8a93bf334662f1584c7ec4e607e4df783ed0c9d
sebastienjean <baz.jean@gmail.com>
2013-09-23 22:54:14 +0200

    Reviewed code, added some TODOs

--- src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
+++ src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
@@ -3,12 +3,14 @@ package fr.iutvalence.java.mp.tictactoe;
 /**
  * TicTacToe
  * 
+ * //TODO (fix) use @version tag
  * v0.0
  * 
  * 2013-09-20
  * 
  * @author seneclap
  *
+ * //TODO (fix) misplaced comment (class comment should be placed before @author)
  * This class is used to start and handle a new game.
  * 
  */
