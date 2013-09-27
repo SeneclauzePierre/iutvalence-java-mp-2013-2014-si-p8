d61be436f54c918127d506047976bd0678630365
SeneclauzePierre <pierre.seneclauze@iut-valence.fr>
2013-09-27 09:58:33 +0200

    Square removed, GameTurn added
    The Square class has been replaced by a more simple integer table.
    A new method, GameTurn, has been added. It is used to change the value
    of one square after a player has played.

--- src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
+++ src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
@@ -20,7 +20,23 @@ public class TicTacToe
      * grid[Ordinate][Abscissa]
      * Game's grid
      */
-    private Square grid[][];
+    private int grid[][];
+    
+    /**
+     * EMPTY
+     * This constant means there is no symbol in a square
+     */
+    public final static int EMPTY = 0;
+    /**
+     * CROSS
+     * This constant means the square contains a cross
+     */
+    public final static int CROSS = 1;
+    /**
+     * CIRCLE
+     * This constant means the square contains a circle
+     */
+    public final static int CIRCLE = 2;
     
     /**
      *  NewGame
@@ -34,8 +50,23 @@ public class TicTacToe
         {
             for (j=1;j<17;j++)
             {
-                this.grid[i][j] = new Square();
+                this.grid[i][j] = TicTacToe.EMPTY;
             }
+        }
+    }
+    
+    /**
+     * GameTurn
+     * Puts the player's symbol in the square located in (x,y)
+     * @param player The number of the player playing this turn
+     * @param x Ordinate of the square in which the player places his symbol
+     * @param y Abscissa of the square in which the player places his symbol
+     */
+    public void GameTurn(int player, int x, int y)
+    {
+        if (this.grid[x][y] == TicTacToe.EMPTY);
+        {
+            //TODO (fix) Complete this method
         }
     }
 }
