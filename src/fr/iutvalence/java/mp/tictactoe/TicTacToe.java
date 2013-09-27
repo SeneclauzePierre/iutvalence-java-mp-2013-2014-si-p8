735998204b2487b6bac86d4032915c0824ae2665
SeneclauzePierre <pierre.seneclauze@iut-valence.fr>
2013-09-27 08:08:26 +0200

    FIXED TODOs

--- src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
+++ src/fr/iutvalence/java/mp/tictactoe/TicTacToe.java
@@ -3,15 +3,15 @@ package fr.iutvalence.java.mp.tictactoe;
 /**
  * TicTacToe
  * 
+ * This class is used to start and handle a new game.
+ * 
  * @version 0.0
  * 
  * 2013-09-20
  * 
- * This class is used to start and handle a new game.
- * 
  * @author seneclap
  *
- * //TODO (fix) misplaced comment (class comment should be placed before any other tag)
+ * //TODO (FIXED) misplaced comment (class comment should be placed before any other tag)
  */
 public class TicTacToe
 {
