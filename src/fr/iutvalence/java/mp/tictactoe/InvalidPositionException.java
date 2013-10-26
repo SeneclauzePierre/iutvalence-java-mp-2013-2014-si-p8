59f2433653431b5c6d939d8c946ebb6dfacca5da
sebastienjean <baz.jean@gmail.com>
2013-10-26 21:00:31 +0200

    Created InvalidPositionException class
    - this exception is used to propagate generic exception related to
    invalid positions (unmarked or out of bounds)
    - PositionOutOfBoundsException is now a subclass of
    InvalidPositionException

---
+++ src/fr/iutvalence/java/mp/tictactoe/InvalidPositionException.java
@@ -0,0 +1,6 @@
+package fr.iutvalence.java.mp.tictactoe;
+
+public class InvalidPositionException extends Exception
+{
+
+}
--- src/fr/iutvalence/java/mp/tictactoe/PositionOutOfBoundsException.java
+++ src/fr/iutvalence/java/mp/tictactoe/PositionOutOfBoundsException.java
@@ -3,5 +3,5 @@ package fr.iutvalence.java.mp.tictactoe;
 /**
  * An exception thrown when a square is called out of the grid's boundaries 
  */
-public class PositionOutOfBoundsException extends Exception
+public class PositionOutOfBoundsException extends InvalidPositionException
 {}
