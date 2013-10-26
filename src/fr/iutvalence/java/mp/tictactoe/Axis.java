866e19b8ede48a335273350c9110c47a12f09697
sebastienjean <baz.jean@gmail.com>
2013-10-26 21:03:16 +0200

    Updated Axis
    - Axis now defines two methods allwong to distinguish a primary and a
    secondary direction (used to mark lines)

--- src/fr/iutvalence/java/mp/tictactoe/Axis.java
+++ src/fr/iutvalence/java/mp/tictactoe/Axis.java
@@ -2,7 +2,7 @@ package fr.iutvalence.java.mp.tictactoe;
 
 /**
  * Enumerate all different axis
- *
+ * 
  */
 public enum Axis
 {
@@ -21,5 +21,35 @@ public enum Axis
     /**
      * represents the up_left-down_right axis
      */
-    UP_LEFT_DOWN_RIGHT;  
+    UP_LEFT_DOWN_RIGHT;
+
+    public Direction getPrimaryDirection()
+    {
+        switch (this)
+        {
+        case UP_DOWN:
+            return Direction.UP;
+        case LEFT_RIGHT:
+            return Direction.RIGHT;
+        case UP_LEFT_DOWN_RIGHT:
+            return Direction.UP_LEFT;
+        default:
+            return Direction.UP_RIGHT;
+        }
+    }
+
+    public Direction getSecondaryDirection()
+    {
+        switch (this)
+        {
+        case UP_DOWN:
+            return Direction.DOWN;
+        case LEFT_RIGHT:
+            return Direction.LEFT;
+        case UP_LEFT_DOWN_RIGHT:
+            return Direction.DOWN_RIGHT;
+        default:
+            return Direction.DOWN_LEFT;
+        }
+    }
 }
